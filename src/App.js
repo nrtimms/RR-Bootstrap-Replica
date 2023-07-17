f203aadf556fd24516bfd1b0e07e1b505563c3b3
nrtimms <nataliertimms18@gmail.com>
2023-07-17 11:54:20 -0700

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import TopBar from './components/TopBar'
+import StoreListing from './components/StoreListing'
+import Background from './components/Background'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Background />
+      <StoreListing />
     </div>
   );
 }
